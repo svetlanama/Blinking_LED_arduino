0178f7a874b7f2fb55e4680c7289faf75e9c4faa
Svitlana Moiseyenko <alexandrovna.sveta@gmail.com>
2017-06-27 02:17:56 +0300

    make corrections

--- sketch_jun23a/sketch_jun23a.ino
+++ sketch_jun23a/sketch_jun23a.ino
@@ -1,32 +1,32 @@
 
 int LED = 12;
-double gcycleTime = 1;//sec
-double cycleTime = 1;//sec
+double gcycleTime = 4; //times
+double cycleTime = 1; //1 sec duration
 
-int longSignal = 0; // 0.06;
+int longSignal = 0;  // 0.06;
 int shortSignal = 1; // 0.03;
 
 int frameSequence = 0.03;
 int current = 0;
- 
+
 void setup() {
-  // put your setup code here, to run once:
+ 
   Serial.begin(9600); // for pront data
   pinMode(LED, OUTPUT);
 
-  while(gcycleTime > 0) {
-   while(cycleTime > 0) {
-    if (cycleTime > 0) {
-      if (current == 0) {
-        sendShortSignal();
+  while (gcycleTime > 0) {
+    while (cycleTime > 0) {
+      if (cycleTime > 0) {
+        if (current == 0) {
+          sendShortSignal();
+        } else {
+          sendLongSignal();
+        }
+        sendBreakSignal();
       } else {
-        sendLongSignal();
+        digitalWrite(LED, LOW);
       }
-       sendBreakSignal();
-    } else {
-       digitalWrite(LED, LOW);
     }
-  }
     gcycleTime --;
     cycleTime = 1;
     digitalWrite(LED, LOW);
@@ -39,41 +39,35 @@ void loop() {
 }
 
 void sendLongSignal() {
-    Serial.println("sendLongSignal ");
+  Serial.println("sendLongSignal ");
 
-    digitalWrite(LED, HIGH);
-    double _delay = frameSequence * 2;
-    delay(_delay);
-    current = longSignal;
-    
-    cycleTime -= _delay;
-    //Serial.println("cycleTime: ");
-    //Serial.println(cycleTime, DEC);
+  digitalWrite(LED, HIGH);
+  double _delay = frameSequence * 2;
+  delay(_delay);
+  current = longSignal;
+
+  cycleTime -= _delay;
 }
 
 
 void sendShortSignal() {
-    Serial.println("sendShortSignal ");
-    
-    digitalWrite(LED, HIGH);
-    double _delay = frameSequence;
-    delay(_delay); 
-    current = shortSignal;
+  Serial.println("sendShortSignal ");
 
-    cycleTime -= _delay;
-    //Serial.println("cycleTime: ");
-    //Serial.println(cycleTime, DEC);
+  digitalWrite(LED, HIGH);
+  double _delay = frameSequence;
+  delay(_delay);
+  current = shortSignal;
+
+  cycleTime -= _delay;
 }
 
 void sendBreakSignal() {
-    Serial.println("sendBreakSignal ");
-    
-    digitalWrite(LED, LOW);
-    double _delay = 0.1;
-    delay(_delay);
-    
-    cycleTime -= _delay;
-    //Serial.println("cycleTime: ");
-    //Serial.println(cycleTime, DEC);
+  Serial.println("sendBreakSignal ");
+
+  digitalWrite(LED, LOW);
+  double _delay = 0.1;
+  delay(_delay);
+
+  cycleTime -= _delay;
 }
 
